701e6bdecc58ea1e9b633d8a46d91b84b202495e
BryanTeves <bryanteves2013@hotmail.com>
2023-04-09 22:16:15 -0300

    not-found function

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+
 function NotFound() {
+  const Navigate = useNavigate();
+
+  const handleClick = () => Navigate("/home");
+
   return (
     <div className="dark-mode not-found">
       <div className="not-found--content">
         <h1>Page not found | Página não encontrada!</h1>
 
-        <button className="btn btn_neon--light-blue not-found--btn">
+        <button
+          onClick={handleClick}
+          className="btn btn_neon--light-blue not-found--btn"
+        >
           Home
         </button>
       </div>
